662ed41823edb993e1616c6c2cc3fa8da4727195
BeomSu_Notebook <qjatn0521@kw.ac.kr>
2023-05-16 21:30:54 +0900

    map

---
+++ app/src/main/java/com/example/testapplication/HomeFragment.kt
@@ -0,0 +1,22 @@
+package com.example.testapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.testapplication.databinding.HomeBinding
+
+class HomeFragment : Fragment(){
+    override fun onCreateView(
+        inflater : LayoutInflater,
+        container : ViewGroup?,
+        savedInstanceState: Bundle?
+
+
+
+
+    ) : View {
+        return HomeBinding.inflate(layoutInflater).root
+    }
+}
